37e927c4fc2ee50da0d4c7cc0e29e9630019f48b
Kevin Njeru <knjeru@gmail.com>
2018-12-07 16:14:01 -0500

    add routing to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabinComponent } from './components/cabin/cabin.component';
+import { SeatComponent } from './components/seat/seat.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabinComponent,
+    SeatComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/app.routing.ts
@@ -0,0 +1,9 @@
+import {Routes} from '@angular/router';
+import { CabinComponent } from './components/cabin/cabin.component';
+
+export const appRoutes: Routes = [
+  {
+    path: '',
+    component: CabinComponent
+  }
+];
