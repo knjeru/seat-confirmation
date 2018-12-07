71ecbc983edd36676e154956f549bc99448d7edb
Kevin Njeru <knjeru@gmail.com>
2018-12-07 16:31:15 -0500

    clean-up naming in build files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lola-seat-confirmation'`, () => {
+  it(`should have as title 'seat-confirmation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lola-seat-confirmation');
+    expect(app.title).toEqual('seat-confirmation');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lola-seat-confirmation!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to seat-confirmation!');
   });
 });
